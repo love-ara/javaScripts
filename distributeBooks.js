bf7b68130be27adfad4476d47f5798a8d944f685
love-ara <aramideotenaike@gmail.com>
2024-04-25 03:05:16 +0100

    commit

--- distributeBooks.js
+++ distributeBooks.js
@@ -1,10 +1,16 @@
 
-function distribute(arr){
-    for (const value of arr){
-     value.book += value.name+"'s book"
- }
+function distribute(members, books) {
+      assignedBooks = [];
+
+    for (const member of members) {
+        const randomIndex = Math.floor(Math.random() * books.length);
+        const selectedBook = books.splice(randomIndex, 1)[0];
+        assignedBooks.push({ member, book: selectedBook });
+    }
+
+    return assignedBooks;
 }
-console.log(distribute(["Emily", "Jack", "Sophia", "Daniel"]))
+
 
 
 module.exports = {distribute};
--- distributeBooks.test.js
+++ distributeBooks.test.js
@@ -1,7 +1,14 @@
 const {distribute} = require("./distributeBooks.js")
 test("Distribute books to a list", ()=>{
-let arr = ["Emily", "Jack", "Sophia", "Daniel"];
-let books = ["a", "b", "c", "d"]
-let answer = distribute(arr, books);
-expect(answer).toBe([""]);
+    // this test can pass cos the book is to be distributed randomly
+    
+const members = ["Emily", "Jack", "Sophia", "Daniel"];
+const books = ["Book A", "Book B", "Book C", "Book D"];
+const answer = distribute(members, books);
+expect(answer).toEqual([
+    { member: 'Emily', book: 'Book C' },
+    { member: 'Jack', book: 'Book D' },
+    { member: 'Sophia', book: 'Book A' },
+    { member: 'Daniel', book: 'Book B' }
+  ]);
 });
